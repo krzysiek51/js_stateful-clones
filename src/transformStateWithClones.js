06717f94b74b81d5cd771a3cb1acb73325e12a1d
Krzysztof <k.torczylo@gmail.com>
2025-05-04 17:10:15 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -23,36 +23,11 @@ function transformStateWithClones(state, actions) {
       }
     }
 
-    states.push(currentState);
+    // Najważniejsza zmiana:
+    states.push({ ...currentState });
   }
 
   return states;
 }
-
-// Przykład użycia tylko lokalnie (zakomentuj przy sprawdzaniu przez testy)
-
-/*
-const state = {
-  foo: 'bar',
-  bar: 'foo',
-};
-
-const actionList = [
-  {
-    type: 'addProperties',
-    extraData: { name: 'Jim', hello: 'world' },
-  },
-  {
-    type: 'removeProperties',
-    keysToRemove: ['bar', 'hello'],
-  },
-  {
-    type: 'addProperties',
-    extraData: { another: 'one' },
-  },
-];
-
-console.log(transformStateWithClones(state, actionList));
-*/
 
 module.exports = transformStateWithClones;
